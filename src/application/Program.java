17c49e15eb2100682faeff817754b704f3c032e9
Matheus Alencar <matheusalencar6942@gmail.com>
2024-12-03 19:49:47 -0300

    find all sellers implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,18 +11,24 @@ public class Program {
 
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== TEST #1: Seller - findById === ");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
-		System.out.println("=== TEST #2: Seller - findByDepartment === ");
+
+		System.out.println("\n=== TEST #2: Seller - findByDepartment === ");
 		Department department = new Department(2, null);
-		List<Seller> list= sellerDao.findByDeparment(department);
+		List<Seller> list = sellerDao.findByDeparment(department);
 		for (Seller obj : list) {
-			System.out.println(obj);			
+			System.out.println(obj);
 		}
-			
+
+		System.out.println("\n=== TEST #3: Seller - findAll === ");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
 	}
 
 }
