19569eb0778ead0a56f109da978c43114dd00f28
Matheus Alencar <matheusalencar6942@gmail.com>
2024-12-03 20:07:40 -0300

    update seller implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,27 +13,29 @@ public class Program {
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
-		System.out.println("=== TEST #1: Seller - findById === ");
-		Seller seller = sellerDao.findById(3);
-		System.out.println(seller);
-
-		System.out.println("\n=== TEST #2: Seller - findByDepartment === ");
-		Department department = new Department(2, null);
-		List<Seller> list = sellerDao.findByDeparment(department);
-		for (Seller obj : list) {
-			System.out.println(obj);
-		}
-
-		System.out.println("\n=== TEST #3: Seller - findAll === ");
-		list = sellerDao.findAll();
-		for (Seller obj : list) {
-			System.out.println(obj);
-		}
 		
-		System.out.println("\n=== TEST #4: Seller - insert === ");
-		Seller newSeller = new Seller(null, "Greg Blue", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Seller inserted! New id = " + newSeller.getId());
+		  System.out.println("=== TEST #1: Seller - findById === "); Seller seller =
+		  sellerDao.findById(3); System.out.println(seller);
+		 
+		  System.out.println("\n=== TEST #2: Seller - findByDepartment === ");
+		  Department department = new Department(2, null); List<Seller> list =
+		  sellerDao.findByDeparment(department); for (Seller obj : list) {
+		  System.out.println(obj); }
+		 
+		  System.out.println("\n=== TEST #3: Seller - findAll === "); list =
+		  sellerDao.findAll(); for (Seller obj : list) { System.out.println(obj); }
+		 
+		  System.out.println("\n=== TEST #4: Seller - insert === "); 
+		  Seller newSeller = new Seller(null, "Greg Blue", "greg@gmail.com", new Date(), 4000.0, department); sellerDao.insert(newSeller);
+		  System.out.println("Seller inserted! New id = " + newSeller.getId());
+		 
+		  System.out.println("\n=== TEST #5: Seller - update === "); 
+		  seller = sellerDao.findById(8);
+		  seller.setName("Max Yellow");
+		  seller.setEmail("max@gmail.com");
+		  sellerDao.update(seller);
+		  System.out.println("Update completed!");
+		 
 	}
 
 }
